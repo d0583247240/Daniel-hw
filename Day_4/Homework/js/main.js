b997e4edf2e5ef851fee97b71788764e7af32cb0
Daniel <d0583247240@gmail.com>
2022-10-23 21:26:56 +0300

    homework

---
+++ Day_4/Homework/js/main.js
@@ -0,0 +1,104 @@
+
+const obj = {};
+obj.fName = "Meir";
+obj.age = 66;
+obj.married = true;
+console.log(obj)
+
+
+
+
+
+
+var string = prompt("please enter a name")
+var newArray = string.split()
+var array = [
+  
+]
+
+const letter1 = {key: 'value'}
+newArray.splice(0,0,letter1)
+
+
+console.log(newArray);
+
+
+
+// toUpperCase. 
+// sort. 
+
+
+
+// const string = 'Daniel Belik'
+// const vowels =  ["a","e","i","o","u"];
+// const arr = string.split("")
+
+// var vowelsFound = [];
+// var cons = [];
+
+// for( var i =0 ; i< arr.length; i++) {
+//   if (vowels.includes(arr[i])){
+//     vowelsFound.push(arr[i])
+//   } else {
+//     cons.push(arr[i])
+//   }
+// }
+
+// alert("Your string contains the following vowels: " + vowelsFound.sort() +" And the following consonants: " + cons.sort() )
+
+
+
+
+
+
+// const input = "Daniel Belik";
+
+// var myArray = []
+// var myArray2 = []
+
+// const shortWord = function (string) {
+//   let text = string;
+//    myArray2 = text.toLowerCase().split("").sort().join(", ");
+//    myArray = text.toLowerCase().split('').sort();
+// };
+// console.log(myArray, myArray2);
+
+// // for (var i = 0; i < myArray.length; i++) {
+// //   if (myArray[i] == 'a' || myArray[i] == 'e' || myArray[i] == 'i' || myArray[i] == 'o' || myArray[i] == 'e')
+// //     myArray.slice(i)
+// // }
+// shortWord(input);
+
+// ;
+
+// //   sort- alphabetic order .
+// //   split- make string into array with (',') between each element
+
+
+
+
+
+
+
+
+
+
+
+// var string = prompt("enter full name")
+
+// var newArray = createArray(string)
+// function createArray(input) {
+//     var array = [];
+
+//     for (i = 0; i <= string; i++) {
+//       array.push(string(i));
+//       console.log(array);
+//     }
+//     return array;
+//   }
+//   console.log(newArray);
+
+
+// var d = function(string) {
+
+// 
---
+++ Day_4/js/main.js
@@ -0,0 +1,64 @@
+if(location.hash = "#first") {
+    location.hash = "#second"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Students = []; // Zev, charlie , Sami, Avi, Barry
+// for(let i = 0; i< 5; i++) {
+//     Students.push("Enter the name for student #" + (i +1))
+// }
+// var niran = "teachers"
+// var adir = "students"
+
+// const teachers [];
+// for ( let i = 1; i< 2; i++) {
+//     teachers.push(prompt("enter the anme for  teacher #" + (i +1)))
+// }
+// function sortAndPrint
+
+
+
+// const Students = []; // Zev, Charli, Sami, Avi, Barry
+// for (let i = 0; i < 5; i++) {
+//   Students.push(prompt("Enter the name for student #" + (i + 1)));
+// }
+// var niran = "teachers"
+// var adir = "students"
+
+// const Teachers = [];
+// for (let i = 0; i < 2; i++) {
+//   Teachers.push(prompt("Enter the name for teacher #" + (i + 1)));
+// }
+
+// function sortAndPrint(arr, role) {
+//   role = role.toUpperCase()
+//   var text = arr.sort().reverse().join(", ");
+//   alert(`The ${arr.length} ${role} are ${text}.`);
+// }
+
+// sortAndPrint(Students, adir);
+
+// console.log(Students);
+// console.log(adir);
+
+// // var arr = ["Adir","Daniel","Yivgeny","Niran","Eli"]
+// // sorted = arr.sort()
+// // sorted.
+
+
